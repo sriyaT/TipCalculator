40059bbf9982f42d41289cd0b83869adc3d3d266
sriyaT <tsriya6@gmail.com>
2020-03-07 00:05:32 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// var num1 = document.getElementById("bill-amount").value;
+//     console.log("num1 - ",num1);
+//     var num2 = document.getElementById("nop").value; 
+//     console.log("num2 - ",num2);
+// function calculate(){
+//     var num1 = document.getElementById("bill-amount").value;
+//     console.log("num1 - ",num1);   
+//     var num2 = document.getElementById("nop").value; 
+//     console.log("num2 - ",num2);
+//     var calculate = num1/num2;
+//     return calculate;
+
+// }
+// calculate();
+// console.log(calculate());
